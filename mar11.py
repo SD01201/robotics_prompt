dd03044195d07319ce1741a75c2e2728effd08db
SD01201 <SanviD592@stemcollegiate.ca>
2025-03-11 13:54:46 -0600

    Create mar11.py

---
+++ mar11.py
@@ -0,0 +1,28 @@
+Replit Day 5:
+I learned how to write else statements.
+
+print("What Avengers character are you?")
+myHulk = input("Do you like to smash things?")
+if myHulk == "yes":
+  print("You are Hulk!")
+else:
+  print("You are not Hulk")
+
+myIronMan = input("Do you like to fly?")
+if myIronMan == "yes":
+  print("You are Iron Man!")
+else:
+  print("You are not Iron Man")
+
+myThor = input("Do you like to throw lightning?")
+if myThor == "yes":
+  print("You are Thor!")
+else:
+  print("You are not Thor")
+
+myHawkeye = input ("Do you like to shoot arrows?")
+if myHawkeye == "yes":
+  print("You are Hawkeye!")
+else:
+  print("You are not Hawkeye")
+  
