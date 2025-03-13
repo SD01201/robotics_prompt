b323859cca7e37eb8a0185960cee41ab5a46d8d1
SD01201 <SanviD592@stemcollegiate.ca>
2025-03-13 08:47:36 -0600

    Create mar13.py

---
+++ mar13/mar13.py
@@ -0,0 +1,12 @@
+Today I learned how to make my own login program
+
+Replit Day 6:
+print("SECURE LOGIN")
+username = input("What is your username?")
+password = input("What is your password?")
+if username == "Sam" and password == "Isuck":
+  print("Welcome Sam!")
+elif username == "Ambika" and password == "Bookman":
+  print("Hey there Ambika!")
+elif username == "Sanvi" and password == "Sandwich":
+  print("Hi Sanvi!")
